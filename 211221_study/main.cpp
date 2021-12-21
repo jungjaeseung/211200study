e8571872c8ea6bf9414b3c83a724cffbc6e73e6a
3Harang <jungjaeseung9475@gmail.com>
2021-12-21 22:10:53 +0900

    second commit

--- 211221_study/main.cpp
+++ 211221_study/main.cpp
@@ -1,10 +1,21 @@
 #include <stdio.h>
 
+int a = 2;
+int b = 3;
+
 int summary(int a, int b) {
 	return a + b;
 }
 
+int multif(int a, int b) {
+	return a * b;
+}
+
+int answer() {
+	return summary(a, b) + multif(a, b);
+}
+
 int main(void) {
-	printf("%d",summary(10,5));
+	printf("%d",answer());
 	return 0;
 }
